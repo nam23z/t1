2f9b388419003edfc1a5a7fe0f4624df343a47fa
nam23z <namnguyencong23@gmail.com>
2023-05-19 14:18:44 +0700

    19/05

--- 19.05/main.js
+++ 19.05/main.js
@@ -1,6 +1,3 @@
-// function details(){
-//     window.location.assign('detail.html')
-// }
 async function getAll() {
     return fetch('https://fakestoreapi.com/products?limit=6')
               .then(res=>res.json());       
